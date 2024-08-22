f31aea694cd41e9de678eef1faf9002d5c8547c1
adflinxy <152524846+adflinxy@users.noreply.github.com>
2024-08-22 22:41:29 +0800

    questions_math.py
    updated it to "abcd" format

--- questions_math.py
+++ questions_math.py
@@ -1,200 +1,208 @@
 questions_math = [
+    # Multiple-Choice Questions
     {
-        'question_text': '(-x^2 + 9x - 2) - 3x(x + 3) + 3x(5)', 
-        'options': [
-            '-2x^2 + 15x - 2', 
-            '-2x^2 + 9x - 2', 
-            '-4x^2 + 18x - 2', 
-            '-4x^2 + 15x - 2'
-            ], 
-            'correct_answer': '-4x^2 + 15x - 2'
-    }, 
+        "question_text": "(-x^2 + 9x - 2) - 3x(x + 3) + 3x(5)",
+        "options": {
+            "a": "-2x^2 + 15x - 2",
+            "b": "-2x^2 + 9x - 2",
+            "c": "-4x^2 + 18x - 2",
+            "d": "-4x^2 + 15x - 2"
+        },
+        "correct_answer": "d"
+    },
     {
-        'question_text': 'Two trucks are traveling towards each other. One with a speed of 30 kph and the other with 40 kph. After how long will the two trucks meet if they are now 140 km apart?', 
-        'options': [
-            '2 hours', 
-            '3 hours', 
-            '4 hours', 
-            '5 hours'
-            ], 
-            'correct_answer': '4 hours'
-    }, 
+        "question_text": "Two trucks are traveling towards each other. One with a speed of 30 kph and the other with 40 kph. After how long will the two trucks meet if they are now 140 km apart?",
+        "options": {
+            "a": "2 hours",
+            "b": "3 hours",
+            "c": "4 hours",
+            "d": "5 hours"
+        },
+        "correct_answer": "c"
+    },
     {
-        'question_text': 'A pride of 7 lions can consume 2 elephant carcasses in 18 days. If two more lions join the pride and this time there are four elephant carcasses, how many days can the pride finish consuming the carcasses?', 
-        'options': [
-            '12 days', 
-            '14 days', 
-            '28 days', 
-            '36 days'
-            ], 
-            'correct_answer': '28 days'
-    }, 
+        "question_text": "A pride of 7 lions can consume 2 elephant carcasses in 18 days. If two more lions join the pride and this time there are four elephant carcasses, how many days can the pride finish consuming the carcasses?",
+        "options": {
+            "a": "12 days",
+            "b": "14 days",
+            "c": "28 days",
+            "d": "36 days"
+        },
+        "correct_answer": "c"
+    },
     {
-        'question_text': 'Find the average of the first 10 prime numbers.', 
-        'options': [
-            '12.9', 
-            '12.5', 
-            '13.0', 
-            '13.9'
-            ], 
-            'correct_answer': '13.0'
-    }, 
+        "question_text": "Find the average of the first 10 prime numbers.",
+        "options": {
+            "a": "12.9",
+            "b": "12.5",
+            "c": "13.0",
+            "d": "13.9"
+        },
+        "correct_answer": "c"
+    },
     {
-        'question_text': 'A frog was at the bottom of a 15-meter well. Each day, it climbed up 3 meters during the day but slid back 1 meter during the night. Approximately, how many days will it take the frog to get out of the well?', 
-        'options': [
-            '7 days', 
-            '8 days', 
-            '9 days', 
-            '10 days'
-            ], 
-            'correct_answer': '8 days'
-    }, 
+        "question_text": "A frog was at the bottom of a 15-meter well. Each day, it climbed up 3 meters during the day but slid back 1 meter during the night. Approximately, how many days will it take the frog to get out of the well?",
+        "options": {
+            "a": "7 days",
+            "b": "8 days",
+            "c": "9 days",
+            "d": "10 days"
+        },
+        "correct_answer": "b"
+    },
     {
-        'question_text': 'Bo and Jo can paint a house in 2 1/3 days. How long will it take Bo to do the job alone if Jo can paint the house alone for 4 1/2 days?', 
-        'options': [
-            '5,000', 
-            '5,200', 
-            '5,625', 
-            '6,500'
-            ], 
-            'correct_answer': '5,625'
-    }, 
+        "question_text": "Bo and Jo can paint a house in 2 1/3 days. How long will it take Bo to do the job alone if Jo can paint the house alone for 4 1/2 days?",
+        "options": {
+            "a": "5,000",
+            "b": "5,200",
+            "c": "5,625",
+            "d": "6,500"
+        },
+        "correct_answer": "c"
+    },
     {
-        'question_text': 'A tank has two pipes. One pipe that can fill it with water in three hours and another pipe that can empty it in six hours. If both pipes are open, how long will it take to fill the empty tank?', 
-        'options': [
-            '6 hours', 
-            '8 hours', 
-            '9 hours', 
-            '10 hours'
-            ], 
-            'correct_answer': '10 hours'
-    }, 
+        "question_text": "A tank has two pipes. One pipe that can fill it with water in three hours and another pipe that can empty it in six hours. If both pipes are open, how long will it take to fill the empty tank?",
+        "options": {
+            "a": "6 hours",
+            "b": "8 hours",
+            "c": "9 hours",
+            "d": "10 hours"
+        },
+        "correct_answer": "d"
+    },
     {
-        'question_text': 'How many three-letter and three-digit plate numbers can LTO release?', 
-        'options': [
-            '26^3 x 10^3', 
-            '26^3 + 10^3', 
-            '26^3 - 10^3', 
-            '26^6'
-            ], 
-            'correct_answer': '26^3 x 10^3'
-    }, 
+        "question_text": "How many three-letter and three-digit plate numbers can LTO release?",
+        "options": {
+            "a": "26^3 x 10^3",
+            "b": "26^3 + 10^3",
+            "c": "26^3 - 10^3",
+            "d": "26^6"
+        },
+        "correct_answer": "a"
+    },
     {
-        'question_text': 'The sum of the digits of a two-digit number is 10. Reversing the digits increases the original number by 36. What is the original number?', 
-        'options': [
-            '28', 
-            '36', 
-            '46', 
-            '64'
-            ], 
-            'correct_answer': '36'
-    }, 
+        "question_text": "The sum of the digits of a two-digit number is 10. Reversing the digits increases the original number by 36. What is the original number?",
+        "options": {
+            "a": "28",
+            "b": "36",
+            "c": "46",
+            "d": "64"
+        },
+        "correct_answer": "b"
+    },
     {
-        'question_text': 'Shawn put P50,000 in the bank at 5 percent simple interest. How much money will he get if he withdraws his money after 10 years?', 
-        'options': [
-            'P50,000', 
-            'P75,000', 
-            'P250,000', 
-            'P250,000'
-            ], 
-            'correct_answer': 'P250,000'
-    }, 
+        "question_text": "Shawn put P50,000 in the bank at 5 percent simple interest. How much money will he get if he withdraws his money after 10 years?",
+        "options": {
+            "a": "P50,000",
+            "b": "P75,000",
+            "c": "P250,000",
+            "d": "P250,000"
+        },
+        "correct_answer": "c"
+    },
     {
-        'question_text': '[5.180 - 0.604 + 0.7200 * 3 / 4 + 0.884] = ?', 
-        'options': [
-            '6.000', 
-            '6.127', 
-            '5.325', 
-            '4.456'
-            ], 
-            'correct_answer': '6.000'
-    }, 
+        "question_text": "[5.180 - 0.604 + 0.7200 * 3 / 4 + 0.884] = ?",
+        "options": {
+            "a": "6.000",
+            "b": "6.127",
+            "c": "5.325",
+            "d": "4.456"
+        },
+        "correct_answer": "a"
+    },
+    # True/False Questions
     {
-        'question_text': 'The evaluation of (1225^2 - 225^2) is 1,450,000.', 
-        'options': [
-            'True', 
-            'False'
-            ], 
-            'correct_answer': 'True'
-    }, 
+        "question_text": "The evaluation of (1225^2 - 225^2) is 1,250,000.",
+        "options": {
+            "a": "True",
+            "b": "False"
+        },
+        "correct_answer": "b"
+    },
     {
-        'question_text': 'The expression [(2x+3)x+(x^2+9x-2)-13x(5)] simplifies to (3x^2 - 53x - 2).', 
-        'options': [
-            'True', 
-            'False'
-            ], 
-            'correct_answer': 'True'
-    }, 
+        "question_text": "The expression [(2x+3)x+(x^2+9x-2)-13x(5)] simplifies to (x^2 - 53x - 2).",
+        "options": {
+            "a": "True",
+            "b": "False"
+        },
+        "correct_answer": "b"
+    },
     {
-        'question_text': 'The expansion of (x-3)^4 results in (x^4 + 12x^3 - 54x^2 + 108x - 81).', 
-        'options': [
-            'True', 
-            'False'
-            ], 
-            'correct_answer': 'True'
-    }, 
+        "question_text": "The expansion of (x-3)^4 results in (x^4 + 12x^3 - 54x^2 + 108x - 81).",
+        "options": {
+            "a": "True",
+            "b": "False"
+        },
+        "correct_answer": "a"
+    },
     {
-        'question_text': 'After buying something for P381.00, selling it to get a profit of P114.30 represents a 30% markup.', 
-        'options': [
-            'True', 
-            'False'
-            ], 
-            'correct_answer': 'True'
-    }, 
+        "question_text": "After buying something for P381.00, selling it to get a profit of P114.30 represents a 30% markup.",
+        "options": {
+            "a": "True",
+            "b": "False"
+        },
+        "correct_answer": "a"
+    },
     {
-        'question_text': 'If it is 2:45 AM in Manila, the time in New Delhi is 12:15 AM.', 
-        'options': [
-            'True', 
-            'False'
-            ], 
-            'correct_answer': 'True'
-    }, 
+        "question_text": "If it is 2:45 AM in Manila, the time in New Delhi is 11:15 AM.",
+        "options": {
+            "a": "True",
+            "b": "False"
+        },
+        "correct_answer": "b"
+    },
     {
-        'question_text': 'Fabini gives jellybeans to his nephews in proportion to their ages, resulting in Peter receiving 12 jellybeans, Billy receiving 20 jellybeans, and Konrad receiving 28 jellybeans.', 
-        'options': [
-            'True', 
-            'False'
-            ], 
-            'correct_answer': 'True'
-    }, 
+        "question_text": "Fabini gives jellybeans to his nephews in proportion to their ages, resulting in Peter receiving 12 jellybeans, Billy receiving 20 jellybeans, and Konrad receiving 28 jellybeans.",
+        "options": {
+            "a": "True",
+            "b": "False"
+        },
+        "correct_answer": "a"
+    },
     {
-        'question_text': 'In a class of 40 students, 19 students are members of only one club (either the dance club or the Glee club).', 
-        'options': [
-            'True', 
-            'False'
-            ], 
-            'correct_answer': 'True'
-    }, 
+        "question_text": "In a class of 40 students, 19 students are members of only one club (either the dance club or the Glee club).",
+        "options": {
+            "a": "True",
+            "b": "False"
+        },
+        "correct_answer": "a"
+    },
     {
-        'question_text': 'A rectangle has a perimeter of (12y^2-2y+18) and a width of (4y^2-y+6). The length of the rectangle is (2y^2 + 3).', 
-        'options': [
-            'True', 
-            'False'
-            ], 
-            'correct_answer': 'True'
-    }, 
+        "question_text": "A rectangle has a perimeter of (12y^2-2y+18) and a width of (4y^2-y+6). The length of the rectangle is (2y^2 + 3).",
+        "options": {
+            "a": "True",
+            "b": "False"
+        },
+        "correct_answer": "a"
+    },
     {
-        'question_text': 'A total of 87 1x1x2 boxes can fit inside a 5x5x7 box.', 
-        'options': [
-            'True', 
-            'False'
-            ], 
-            'correct_answer': 'True'
-    }, 
+        "question_text": "A total of 75 1x1x2 boxes can fit inside a 5x5x7 box.",
+        "options": {
+            "a": "True",
+            "b": "False"
+        },
+        "correct_answer": "b"
+    },
     {
-        'question_text': 'Mang Joses average earnings over five days (Monday to Friday) are P360.', 
-        'options': [
-            'True', 
-            'False'
-            ], 
-            'correct_answer': 'True'
-    }, 
+        "question_text": "Mang Joses average earnings over five days (Monday to Friday) are P360.",
+        "options": {
+            "a": "True",
+            "b": "False"
+        },
+        "correct_answer": "a"
+    },
     {
-        'question_text': 'Jabee eats 80 burgers during a 20-hour burger-eating contest, where he eats a burger every 15 minutes.', 
-        'options': [
-            'True', 
-            'False'
-            ], 
-            'correct_answer': 'True'
+        "question_text": "Jabee eats 96 burgers during a 20-hour burger-eating contest, where he eats a burger every 15 minutes.",
+        "options": {
+            "a": "True",
+            "b": "False"
+        },
+        "correct_answer": "b"
     }
 ]
+
+def check_answer(question, selected_option):
+    if question["correct_answer"] == selected_option:
+        return "Correct!"
+    else:
+        return "Incorrect."
